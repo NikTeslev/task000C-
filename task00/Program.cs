9d2832886cea79d2d09b100ddf4598b22446279d
NicolaTes <kteslev@mail.ru>
2022-10-18 09:04:03 +0300

    find task00

--- task00/Program.cs
+++ task00/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. принимает на вход
+// выдаёт 
+// число умноженное на себя
+
+
+
+Console.WriteLine("Введите какое-то число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int square = number * number;
+Console.WriteLine($"X*X={square}");
