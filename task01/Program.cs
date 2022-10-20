dbc182313066969cfc69b8b66d13c8e1cac80ae4
NicolaTes <kteslev@mail.ru>
2022-10-20 08:50:18 +0300

    набросал задачи с семинара

---
+++ task01/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите два числа:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int kvadrat = number1 * number1;
+Boolean(number2 = kvadrat);
+if(number2 == kvadrat);
+{
+Console.WriteLine(Yes);
+}
+else
+{
+Console.WriteLine(No);
+}
