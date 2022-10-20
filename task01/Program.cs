1f583844698d97a9589d3b57d60bfecbfb52fafa
NicolaTes <kteslev@mail.ru>
2022-10-20 17:07:52 +0300

    Сделал задачу 21. результаты совпадают

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,13 +1,12 @@
-﻿Console.WriteLine("Введите два числа:");
+﻿Console.Write("Введите два числа:");
 int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
 int kvadrat = number1 * number1;
-Boolean(number2 = kvadrat);
-if(number2 == kvadrat);
+if(number2 == kvadrat)
 {
-Console.WriteLine(Yes);
+Console.WriteLine("Yes");
 }
 else
 {
-Console.WriteLine(No);
+Console.WriteLine("No");
 }
--- task02/Program.cs
+++ task02/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int kvadrat = number1 * number1;
+if(number2 == kvadrat)
+{
+Console.WriteLine("Yes");
+}
+else
+{
+Console.WriteLine("No");
+}
