44c3efd48c54d1250cb70b9bb54fbf065b239088
NicolaTes <kteslev@mail.ru>
2022-11-13 10:39:29 +0300

    пушу до 3го семинара включительно

---
+++ task15/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+Console.WriteLine("Введите номер дня недели:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (number > 0 && number < 8)
+{
+    if (number <= 5)
+    {
+        Console.WriteLine("Рабочий день");
+    }
+    else Console.WriteLine("Выходной");
+}
+else
+{
+     Console.WriteLine("Такого дня недели нет");
+}
--- task19/Program.cs
+++ task19/Program.cs
@@ -8,17 +8,17 @@
 // 23432 -> да
 
 Console.WriteLine("Введите пятизначное число: ");
-int num = Convert.ToInt32(Console.ReadLine());
+int number = Convert.ToInt32(Console.ReadLine());
 
 
-if (num >= 10000 && num <= 99999)
+if (number >= 10000 && number <= 99999)
 {
-    int num1 = num / 10000; // 1
-    int num2 = num / 1000 % 10; // 2
-    int num4 = num / 10 % 10; // 4
-    int num5 = num % 10;
+    int num1 = number / 10000; 
+    int num2 = number / 1000 % 10;
+    int num4 = number / 10 % 10; 
+    int num5 = number % 10;
 
     if (num1 == num5 && num2 == num4) Console.WriteLine("Да");
     else Console.WriteLine("Нет");
 }
-else Console.WriteLine("Введено не пятизначное число");
+else Console.WriteLine("Число не является пятизначным");
