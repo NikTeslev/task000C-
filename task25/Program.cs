a8a68bd39a7f83eca6ab5d5ecbbba72fb2903266
NicolaTes <kteslev@mail.ru>
2022-11-13 12:32:11 +0300

    сделал 25 и 27 задачу

---
+++ task25/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.WriteLine("Введите два числа");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+ if(number2 < 0 )
+    {
+       number2 = number2 * -1;
+    }
+
+int ResNumber(int num1, int num2)
+{
+    int result = 1;
+   
+    
+       for (int i = 1; i <= num2; i++)
+        {
+           
+           result = result * num1;
+        }
+        
+    return result;
+
+}
+int resNumber = ResNumber(number1, number2);
+System.Console.WriteLine(resNumber);
---
+++ task27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+ int num = Convert.ToInt32(Console.ReadLine());
+
+ int SumDigit(int numDigit)
+ {
+     int count = 0;
+     while (numDigit > 0)
+     {
+        // numDigit = numDigit % 10;
+         count = count + (numDigit % 10);
+         numDigit = numDigit / 10;
+     }
+     return count;
+ }
+ int sumDigit = SumDigit(num);
+ Console.WriteLine($"Cумма цифр в числе = {sumDigit}");
+
