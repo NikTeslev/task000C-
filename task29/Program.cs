c13a0f3d91152fd399f3ee41c0939ff4b79f8bc1
NicolaTes <kteslev@mail.ru>
2022-11-14 09:27:32 +0300

    Загружаю задачи 25,27,29

--- task29/Program.cs
+++ task29/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+//1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+
+int[] NameArray(int size)
+{
+    int[] array = new int[size]; 
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{   
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length-1) Console.Write(",");
+    }
+    Console.Write("]");
+}
+
+int[] nameArray = NameArray(8);
+PrintArray(nameArray);
