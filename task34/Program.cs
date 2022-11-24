44eaaeb76d150b514dccfb01b8801c436b109848
NikolaTes <kteslev@mail.ru>
2022-11-24 09:45:18 +0300

    push tasks 34, 36, 38

--- task34/Program.cs
+++ task34/Program.cs
@@ -1,21 +1,44 @@
-﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+﻿// Задача 34: Задайте массив заполненный случайными положительными 
+//трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 
 // [345, 897, 568, 234] -> 2
 
-int[] NewArray(int size);
+int[] NewArray(int size, int min, int max)
 {
     int[] array = new int[size];
     Random rnd = new Random();
-    for (int i = 0; i < array.length; i++)
+    for (int i = 0; i < size; i++)
     {
-        array[i] = rnd.Next(99, 1000);
-    } 
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
 
-}
-void PrintArray(int array);
-Console.Write("[");
-for (int i = 0; i < array.length; i++)
+int QuantytyOdd(int[] array)
 {
-    Console.Write($"{array[i]}");
+    int count = default;
+    for (int i = 0; i < array.Length; i++)
+    {
+       if (array[i] % 2 ==0)
+       {
+        count++;
+       } 
+    }
+    return count;
 }
-Console.Write($"{array[i]}");
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]},");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] arr = NewArray(10, 100, 999);
+PrintArray(arr);
+int quant = QuantytyOdd(arr);
+System.Console.WriteLine($"Количество Четных в массиве ={quant}");
--- task38/Program.cs
+++ task38/Program.cs
@@ -1,3 +1,51 @@
-﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+﻿// Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
 
 // [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+double[] CreateArrayRnd(int size, int min, int max)
+  {
+      double[] array = new double[size];
+      Random rnd = new Random();
+      for (int i = 0; i < size; i++)
+      {
+          double num = rnd.NextDouble() * (max - min) + min;
+          array[i] = Math.Round(num, 2);
+      }
+      return array;
+  }
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]} |");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+double DiffOfMinAndMax(double[] array)
+{
+    double diff = default;
+    double min = array[0];
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] <min)
+        {
+            min = array[i];
+        }
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    
+    }
+    diff = max - min;
+    return diff;
+}
+double[] arr = CreateArrayRnd(6, 1, 9);
+PrintArray(arr);
+double difference = DiffOfMinAndMax(arr);
+System.Console.WriteLine($"Разница между максимальным и минимальным числом равна - {difference}");
